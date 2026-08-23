a9c2bc990ef8ce5fec6a5cbdaa6f75aeae24e585
sebastianvasquez1709-svg <sebastian.vasquez1709@gmail.com>
2026-08-22 20:46:28 -0400

    build: move Vercel build steps into script

---
+++ scripts/vercel-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eu
+node scripts/patch-dispatch-print.js
+node scripts/patch-dispatch-print-v3.js
+node scripts/patch-dispatch-ux-v5.js
+rm -rf public
+mkdir -p public
+find . -maxdepth 1 -type f ! -name 'vercel.json' -exec cp -p {} public/ \;
+if [ -d docs ]; then cp -R docs public/; fi
